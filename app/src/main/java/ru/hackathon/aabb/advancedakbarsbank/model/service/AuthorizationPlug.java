1ffae6b1773a1cb7eac595470fbef0aecd5bc24d
Damir <damirilyasov1997@gmail.com>
2017-03-03 01:43:47 +0300

    Code plug added.

--- app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/model/service/AuthorizationPlug.java
+++ app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/model/service/AuthorizationPlug.java
@@ -1,11 +1,20 @@
 package ru.hackathon.aabb.advancedakbarsbank.model.service;
 
 public class AuthorizationPlug {
-    public boolean authorize(String login, String password) {
+    public boolean authorizeByData(String login, String password) {
         if (login.equals("Admin") && password.equals("123")) {
             return true;
         } else {
             return false;
         }
     }
+
+    public boolean authorizeByCode(String code) {
+        if (Integer.parseInt(code) == 00000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
