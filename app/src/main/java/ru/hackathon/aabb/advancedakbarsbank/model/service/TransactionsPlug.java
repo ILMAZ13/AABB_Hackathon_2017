d0a347c04a8a70f25fc55ff607d30004b7b25b00
Damir <damirilyasov1997@gmail.com>
2017-03-03 08:17:59 +0300

    Time to transactions added.

--- app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/model/service/TransactionsPlug.java
+++ app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/model/service/TransactionsPlug.java
@@ -10,15 +10,18 @@ public class TransactionsPlug {
     List<Transaction> getTransactions(){
         List<Transaction> transactions = new ArrayList<>();
 
+        java.sql.Timestamp firstTimestamp = java.sql.Timestamp.valueOf("2017-03-03 10:10:10.0");
+        java.sql.Timestamp secondTimestamp = java.sql.Timestamp.valueOf("2017-04-04 10:10:10.0");
+
         SimpleTransactionGetting firstTransactionGetting = new SimpleTransactionGetting();
         firstTransactionGetting.setCost(50000);
         firstTransactionGetting.setTransactionName("salary");
-        firstTransactionGetting.setTransactionDateAndTime(new java.sql.Timestamp(System.currentTimeMillis()));
+        firstTransactionGetting.setTransactionDateAndTime(firstTimestamp);
 
         SimpleTransactionGetting secondTransactionGetting = new SimpleTransactionGetting();
         secondTransactionGetting.setTransactionName("friend's payment");
         secondTransactionGetting.setCost(1000);
-        secondTransactionGetting.setTransactionDateAndTime(new java.sql.Timestamp(System.currentTimeMillis()));
+        secondTransactionGetting.setTransactionDateAndTime(secondTimestamp);
 
         transactions.add(firstTransactionGetting);
         transactions.add(secondTransactionGetting);
