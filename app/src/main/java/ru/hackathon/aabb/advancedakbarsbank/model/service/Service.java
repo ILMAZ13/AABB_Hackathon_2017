232689fa38c321732c00fd05dbb4db0713640a84
Азат Хайруллин < >
2017-03-04 01:29:56 +0300

    Azat: Service modified and TransactionPresenterAdded

--- app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/model/service/Service.java
+++ app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/model/service/Service.java
@@ -1,31 +1,35 @@
 package ru.hackathon.aabb.advancedakbarsbank.model.service;
 
 import ru.hackathon.aabb.advancedakbarsbank.model.enums.TransactionTypeEnum;
+import ru.hackathon.aabb.advancedakbarsbank.model.interfaces.Payment;
 import ru.hackathon.aabb.advancedakbarsbank.model.interfaces.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Service {
-    private List<Transaction> transactions;
+    private List<Transaction> transactions= new ArrayList<>();
+    private List<Payment> payments = new ArrayList<>();
 
     public void setTransactions(List<Transaction> transactions) {
         this.transactions = transactions;
+    }
+
+    public void setPayments(List<Payment> payments) {
+        this.payments = payments;
     }
 
     public List<Transaction> getTransactions() {
         return transactions;
     }
 
-    public void transactionAdding(Transaction transaction) {
-        transactions.add(transaction);
+    public List<Payment> getPayments() {
+        return payments;
     }
 
-    public int getTransactionSum(TransactionTypeEnum transactionTypeEnum) {
-        int summ = 0;
-        for (Transaction transaction : transactions) {
-            if (transaction.getTransactionTypeEnum().equals(transactionTypeEnum))
-                summ += transaction.getMoney();
-        }
-        return summ;
+    public void transactionAdding(Transaction transaction) {
+        transactions.add(transaction);
+        if(transaction.getTransactionTypeEnum().equals(TransactionTypeEnum.PAYMENT))
+            payments.add((Payment) transaction);
     }
 }
