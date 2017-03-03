20fa557077c3050246aef484c3972a0123d1b53e
ILMAZ13 <ilmaz1309@gmail.com>
2017-03-03 19:26:25 +0300

    Added application

---
+++ app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/AuthorithationActivity.java
@@ -0,0 +1,13 @@
+package ru.hackathon.aabb.advancedakbarsbank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthorithationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorithation);
+    }
+}
