c501106a1e4cbc38dd6cfcc7cc951ffdea068288
Damir <damirilyasov1997@gmail.com>
2017-03-03 15:55:26 +0300

    renamed File

--- app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/view/fragments/HistoryFragment.java
+++ app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/view/fragments/HistoryFragment.java
@@ -1,15 +1,15 @@
 package ru.hackathon.aabb.advancedakbarsbank.view.fragments;
 
-import java.util.List;
-
 import ru.hackathon.aabb.advancedakbarsbank.model.interfaces.Transaction;
 import ru.hackathon.aabb.advancedakbarsbank.view.interfaces.StoryFragmentInterface;
+
+import java.util.List;
 
 /**
  * Created by ILMAZ on 04.03.2017.
  */
 
-public class StoryFragmaent implements StoryFragmentInterface {
+public class HistoryFragment implements StoryFragmentInterface {
     @Override
     public void setTransactionsList(List<Transaction> list) {
 
