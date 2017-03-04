54f7c5ebebc528e0896e9d7c7bae75e7218993ec
ILMAZ13 <ilmaz1309@gmail.com>
2017-03-04 15:59:27 +0300

    Added interface

--- app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/view/fragments/StoryFragmaent.java
+++ app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/view/fragments/StoryFragmaent.java
@@ -1,8 +1,17 @@
 package ru.hackathon.aabb.advancedakbarsbank.view.fragments;
 
+import java.util.List;
+
+import ru.hackathon.aabb.advancedakbarsbank.model.interfaces.Transaction;
+import ru.hackathon.aabb.advancedakbarsbank.view.interfaces.StoryFragmentInterface;
+
 /**
  * Created by ILMAZ on 04.03.2017.
  */
 
-public class StoryFragmaent {
+public class StoryFragmaent implements StoryFragmentInterface {
+    @Override
+    public void setTransactionsList(List<Transaction> list) {
+
+    }
 }
