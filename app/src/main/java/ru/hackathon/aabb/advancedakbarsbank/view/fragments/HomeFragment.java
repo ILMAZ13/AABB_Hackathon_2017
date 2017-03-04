04084c77989ab388aac97d62060914905afe4441
ILMAZ13 <ilmaz1309@gmail.com>
2017-03-04 06:16:55 +0300

    Added card item

--- app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/view/fragments/HomeFragment.java
+++ app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/view/fragments/HomeFragment.java
@@ -2,7 +2,14 @@ package ru.hackathon.aabb.advancedakbarsbank.view.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toolbar;
 
+import ru.hackathon.aabb.advancedakbarsbank.R;
 import ru.hackathon.aabb.advancedakbarsbank.model.entity.foruser.User;
 import ru.hackathon.aabb.advancedakbarsbank.view.interfaces.HomeFragmentInterface;
 
@@ -12,6 +19,8 @@ import ru.hackathon.aabb.advancedakbarsbank.view.interfaces.HomeFragmentInterfac
 
 public class HomeFragment extends Fragment implements HomeFragmentInterface {
     private User mUser;
+    private Toolbar mToolbar;
+    private RecyclerView mRecyclerView;
     public static HomeFragment newInstance() {
 
         Bundle args = new Bundle();
@@ -21,7 +30,22 @@ public class HomeFragment extends Fragment implements HomeFragmentInterface {
         return fragment;
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mToolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        getActivity().setActionBar(mToolbar);
+        getActivity().setTitle("АК БАРС БАНК");
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.rv_cart_list);
+        return view;
+    }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 
     @Override
     public void setUserInformation(User user) {
---
+++ app/src/main/java/ru/hackathon/aabb/advancedakbarsbank/view/viewHolders/HomeCardViewHolder.java
@@ -0,0 +1,21 @@
+package ru.hackathon.aabb.advancedakbarsbank.view.viewHolders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import ru.hackathon.aabb.advancedakbarsbank.model.entity.foruser.Card;
+
+/**
+ * Created by ILMAZ on 04.03.2017.
+ */
+
+public class HomeCardViewHolder extends RecyclerView.ViewHolder {
+
+    public HomeCardViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    public void bind(Card card){
+
+    }
+}
